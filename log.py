94212e90e2ad13bc5b7ca6cc97622ea5489e946e
Kostya <kostilio911@gmail.ru>
2022-12-22 20:35:38 +0400

    remove config

--- log.py
+++ log.py
@@ -4,6 +4,6 @@ import datetime
 
 
 def log(update: Update, context: CallbackContext):
-    with open('log.csv', 'a') as file:
+    with open('log.csv','a',encoding = 'UTF-8') as file:
         file.write(f'{datetime.datetime.now().time()}|{update.effective_user.first_name}|{update.effective_user.id}|'
                    f'{update.message.text}\n')
